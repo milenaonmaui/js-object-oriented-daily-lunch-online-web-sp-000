78e37f569e99122b0614fa9fc70d3a7730b65388
Milena Kari <milenakari@yahoo.com>
2019-08-22 22:13:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,88 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
+
+let neighborhoodId = 0;
+let customerId = 0;
+let mealId = 0;
+let deliveryId = 0;
+
+class Neighborhood {
+    constructor(name){
+        this.name = name
+        this.id = ++neighborhoodId;
+        store.neighborhoods.push(this);
+    }
+    deliveries(){
+
+    }
+    customers(){
+
+    }
+    meals(){
+
+    }
+}
+
+class Customer{
+    constructor(name, neighborhoodId){
+        this.name=name;
+        this.neighborhoodId=neighborhoodId;
+        this.id = ++customerId;
+        store.customers.push(this);
+    }
+
+    deliveries(){
+
+    }
+    meals() {
+        
+    }
+    totalSpent(){
+
+    }
+}
+
+class Meal {
+    constructor(title, price){
+        this.title = tilte;
+        this.price = price;
+        this.id = ++mealId;
+        store.meals.push(this);
+    }
+    deliveries() {
+        
+    }
+    customers() {
+    
+    }
+    byPrice() {
+
+    }
+}   
+
+class Delivery {
+    constructor(mealId, neighborhoodId, customerId){
+        this.mealId = mealId;
+        this.neighborhoodId=neighborhoodId;
+        this.customerId = customerId;
+        this.id = ++deliveryId;
+        store.deliveries.push(this);
+    }
+
+    meal(){
+        return store.meals.find(meal => {
+            return meal.id === this.mealId;
+        });
+    }
+    customer() {
+        return store.customers.find(customer => {
+            return customer.id === this.customerId;
+        });
+    }
+    neighborhood(){
+        return store.neighborhoods.find(neighborhood => {
+            return neighborhood.id === this.neighborhoodId;
+        });
+
+    }
+}
